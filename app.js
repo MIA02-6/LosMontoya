44a230b17cfd0f603fddc80bd1eb73237e60e323
MIA02-6 <mia15condori@gmail.com>
2025-12-06 23:55:40 -0500

    Update API endpoint to local server

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Frontend JS: fetches products from backend, cart logic, banners and checkout demo
-const API = "/api"; // when deployed, proxy or host backend separately
+const API = "http://127.0.0.1:8000/api";
+ // when deployed, proxy or host backend separately
 
 const banners = [
   {id:1, title:"Promo - 2x1 Pollo", img:"images/banner1.jpg"},
